66d5e8f794f1a123aea753f56f6bcd2294581584
Illya <121565465+IllyaEpik@users.noreply.github.com>
2025-04-28 21:20:07 +0300

    something new

--- create_app/static/js/create.js
+++ create_app/static/js/create.js
@@ -13,14 +13,22 @@ function saveQuestion() {
     const question = document.getElementById('question').value;
     const correctAnswer = document.getElementById('correctAnswer').value;
     const options = Array.from(document.getElementsByClassName('option')).map(input => input.value);
-
+    console.log('1312123132132')
     fetch('/save_question', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ question, correct_answer: correctAnswer, options: options })
+        body: JSON.stringify({ question: question, correct_answer: correctAnswer, options: options })
     })
     .then(response => response.json())
     .then(data => {
         alert(data.message);
+        // alert(response)
+        // console.log(0/0/0/0/0)
     });
+    alert(data.message)
 }
+// document.getElementById('add').addEventListener(() => addOption)
+// document.createElement()
+document.querySelector('#save').addEventListener('click',saveQuestion)
+console.log()
+// 
--- create_app/views.py
+++ create_app/views.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 import flask, flask_login
-
-def render_create():
-    # nickname = flask_login.current_user.nickname 
-=======
 import flask
 def render_create():
-
-    
->>>>>>> origin/Mark
     return flask.render_template("create.html")
