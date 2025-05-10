36bf43bf08db0e28b3695fa5089ff604e380aa49
Illya <121565465+IllyaEpik@users.noreply.github.com>
2025-05-10 21:59:02 +0300

    load

--- create_app/static/js/create.js
+++ create_app/static/js/create.js
@@ -35,11 +35,33 @@ document.getElementById("add_question").addEventListener("click", function () {
             localStorage.setItem(`${number}`, JSON.stringify(
                 question
             ))
-            console.log(localStorage.getItem(`${number}`))
-            questionForm.querySelector("#question").value=""
-            questionForm.querySelector("#correctAnswer").value=""
-            questionForm.querySelector("#options").innerHTML=""
-            
+            // questionCount ``
+
+            console.log(localStorage.getItem(`${questionCount}`))
+            let data = JSON.parse(localStorage.getItem(`${questionCount}`))
+            // let options = ''
+            questionForm.querySelector("#question").value = data['question']
+            questionForm.querySelector("#correctAnswer").value= data['correct']
+            questionForm.querySelector("#options").innerHTML = ''
+            for (let option of data['options']){
+                let div = document.createElement('div')
+                let input = document.createElement('input')
+                let button = document.createElement('button')
+                input.value = option
+                input.type = 'text'
+                input.classList.add('option')
+                input.placeholder = 'Новий варіант'
+                button.classList.add('remove_option')
+                button.name = 'answer'
+                button.type = 'button'
+                button.textContent = '➖'
+                div.append(input)
+                div.append(button)
+                questionForm.querySelector("#options").append(div)
+                // options += "<div><button type='button' class='remove_option' name='answer'>➖</button></div>"
+            }
+            // questionForm.querySelector("#options").innerHTML=options Previous.Hello.Happy BirthdayHello Kivara.Hello.Hello, Satada. Doll.Kukala.Привет капибара пиши давай.Вау, вот он понимает.Это асфальтная дичь, понимаешь, что так хотя бы по пожалуйста.Всё стоя, что мы занимаемся?
+            // console.log(questionForm.querySelector("#options").innerHTML)
         }
 
     });
@@ -50,11 +72,17 @@ document.getElementById("add_question").addEventListener("click", function () {
 document.getElementById("delete_question").addEventListener("click", function () {
     const listQuestions = document.querySelector(".list_questions");
     if (listQuestions.children.length > 0) {
-        listQuestions.removeChild(listQuestions.lastElementChild);
+        for (let question of listQuestions.children ){
+            if (question.classList.contains('question_button_choosen')){
+                listQuestions.removeChild(question)
+
+            }
+        }
+        // listQuestions.removeChild(listQuestions.lastElementChild);
     } else {
         alert("Список тестів порожній!");
     }
-
+    // 
     questionForm.reset(); 
     questionForm.querySelector("h3").textContent = "Питання:"; 
 });
