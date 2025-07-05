eee0330cedac41b8cfe5c267f6861111cfe802ce
Illya <121565465+IllyaEpik@users.noreply.github.com>
2025-07-05 21:29:49 +0300

    something

--- check/script.js
+++ check/script.js
@@ -1,11 +1,43 @@
+// создаем и сохраняем <p></p> в константу квас
+const kwas = document.createElement("p")
+// присваиваем ид
+kwas.id = 'kwas' 
+// указываем содержание <p></p>
+kwas.textContent = 'kwas'
+// указиваем классы для <p></p>
+kwas.className = 'kwas lol'
+// добавляем класс kwasik  
+kwas.classList.add('kwasik')
+// создаем триггер на клик 
+kwas.addEventListener('click', function (){
+    // выводим kwasssss в консоль
+    console.log('kwasssss')
+})
 
-let stakan = document.querySelector(".beer") // получаем елемент по класу для того что бы получить по айди нужна решетка #
-let vodka = document.createElement("p") // создаем елемент p з также можно с div 
+// получаем стакан из документа
+let stakan = document.querySelector('.stakan')
+// добавляем в стакан квас 
+stakan.append(kwas)
 
-vodka.textContent = "cocain" // задаем текстовы контент 
-vodka.className = 'cocain' // создаем класс с названием кокаин
-vodka.id = "cocain" // создаем id с названием кокаин
 
-vodka.style.color = 'purple' // задаем пурпурный цвет тексту
 
-stakan.append(vodka) // добавляем елемент в div
+// получаем html елемент картинки без картинки 
+let img = document.querySelector('img')
+// получаем input который получает файлы 
+let inp = document.querySelector('#fileInput')
+// позволяет читать файлы
+let reader = new FileReader()
+
+// указивыем что будет происходить при загрузке картинки 
+reader.onload = function(){
+    // закидываем картинку в тег картинки (отображаем картинку)
+    img.src = reader.result
+}
+
+// указываем что будет происходить при изменеии картинки в input
+inp.addEventListener('change', function (event){
+    // выводим данные картинки в терминал
+    console.log(inp.files[0])
+    // читаем файл читатилем
+    reader.readAsDataURL(inp.files[0])
+} )
