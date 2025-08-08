51b25985e15016e195c9f85bcb0aaf5372e07192
Illya <121565465+IllyaEpik@users.noreply.github.com>
2025-08-08 20:18:49 +0300

    ok

---
+++ create_app/static/js/main.js
@@ -0,0 +1,5 @@
+
+import removeElems from "./modules/remove.js";
+import change from "./modules/change.js";
+change()
+removeElems()
---
+++ create_app/static/js/modules/add.js
@@ -0,0 +1,7 @@
+// addQuestion
+{/* <div class="questionContainer"><button class="button questionButton">Питання 1 <img src="{{ url_for('create.static', filename='images/bin.svg') }}" alt="" class="add-question-icon removeButton"></button></div> */}
+function add(){
+    let addQuestion = document.querySelector('#addQuestion')
+    let questionContainer = document.querySelector('.questionContainer')
+    // (<button class="button questionButton">Питання 1 <img src="{{ url_for('create.static', filename='images/bin.svg') }}" alt="" class="add-question-icon removeButton"></button>)
+}
