0c623cb54b279aaa932ff3059fe455f22b343cc2
Пользователь <shornikov.oleksandr@gmail.com>
2025-09-08 23:15:05 +0300

    ok

--- create_app/static/js/modules/save.js
+++ create_app/static/js/modules/save.js
@@ -1,40 +1,39 @@
 function save() {
     let select = document.querySelector('.select')
-    // select = select ? select : document.querySelector("#settingButton")
-    // questionForTest
     if (select.id != "settingButton"){
-        // let questions = /
-        let selectImg = document.querySelector(".selectImg").parentElement
-        let count = 0
+        let selectImgEl = document.querySelector(".selectImg");
+        let selectImgParent = selectImgEl ? selectImgEl.parentElement : null
+
         let saveData = {
             "question":document.querySelector("#questionNameInput").value,
             'answers':[],
             "correct":[],
             "type":"OneAnswerQuestion"
         }
-        if (selectImg.id){
-            saveData["type"] = selectImg.id
+
+        if (selectImgParent){
+            if (selectImgParent.id == 'questionType'){
+                let sel = selectImgParent.querySelector('select')
+                saveData["type"] = sel && sel.value == "2" ? "multipleQuestion" : "OneAnswerQuestion"
+            } else {
+                saveData["type"] = selectImgParent.id
+            }
         }
-        
-        let answer;
+
+        let answer
         for (let question of document.querySelectorAll(".questionForTest")){
-            console.log(question)
             answer = question.querySelector("textarea").value
-            question.querySelector(".isAnswerCorrect").checked ? saveData.correct.push(answer) : null
+            if (question.querySelector(".isAnswerCorrect").checked){
+                saveData.correct.push(answer)
+            }
             saveData.answers.push(answer)
-            count++
         }
         if (saveData["type"]=="OneAnswerQuestion"){
-            // if (saveData['correct']){
             saveData['correct'] = saveData['correct'][0]
-            // }
         }
-        console.log(saveData)
         localStorage.setItem(select.querySelector(".questionNumber").textContent,JSON.stringify(saveData))
     }else{
-
         let objectsToSave = ['class', 'testNameInput', 'subject', 'description']
-        // document.querySelector('#class').value
         let data = {}
         for (let object of objectsToSave){
             data[object] = document.querySelector(`#${object}`).value
